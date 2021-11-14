3c64765d296429ab1561cc3301ccc82f65f6ecf3
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-15 09:54:00 +1100

    🎉 Update

--- src/contracts/Icons.sol
+++ src/contracts/Icons.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "solidity-util/lib/Strings.sol";
+import "./lib/Strings.sol";
 
 contract Icons is Ownable, ERC1155, ChainlinkClient {
     using SafeMath for uint256;
