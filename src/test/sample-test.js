89b054d0deb93c46af15758e1b11ee33796478b3
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-15 10:40:07 +1100

    🎉 Update

--- src/test/sample-test.js
+++ src/test/sample-test.js
@@ -1,8 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-    it("Should return the new greeting once it's changed", async function () {
+describe("Icons", function () {
+    it("Should deploy the contract", async function () {
         const Greeter = await ethers.getContractFactory("Greeter");
         const greeter = await Greeter.deploy("Hello, world!");
         await greeter.deployed();
