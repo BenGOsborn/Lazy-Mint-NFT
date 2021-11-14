e596c462f0d3e5bd7ead2caa171104b328e02ae9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-15 10:38:31 +1100

    🎉 Update

--- src3/test/1-chainlink.test.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import { ethers } from "hardhat";
-import chai from "chai";
-import { solidity } from "ethereum-waffle";
-
-chai.use(solidity);
-const { expect } = chai;
-
-describe("Chainlink", () => {});
