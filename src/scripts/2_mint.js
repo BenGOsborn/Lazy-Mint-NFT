ce0544841c41d184a0abfa2fce74b42ebb0cae5e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-15 13:28:57 +1100

    🎉 Update

--- src/scripts/2_mint.js
+++ src/scripts/2_mint.js
@@ -4,7 +4,8 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
-const ERC20Abi = require("@openzeppelin/contracts/build/contracts/ERC20.json");
+const ERC20ABI = require("@openzeppelin/contracts/build/contracts/ERC20.json");
+const IconsABI = require("../artifacts/contracts/Icons.sol/Icons.json");
 
 async function main() {
     // Hardhat always runs the compile task when running scripts with its command
@@ -18,16 +19,20 @@ async function main() {
     const LINK_ADDRESS = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 
     // Fund the contract with LINK
-    const link = new hre.ethers.Contract(LINK_ADDRESS, ERC20Abi, hre.ethers.provider);
+    const link = new hre.ethers.Contract(LINK_ADDRESS, ERC20ABI.abi, hre.ethers.provider);
     await link.transfer(icons.address, 4e18);
 
     // Initialize the contract
+    const icons = new hre.ethers.Contract(IconsABI.address, IconsABI.abi, hre.ethers.provider);
 
     // Mint a token
     const fee = await icons.mintFee();
     await icons.earlyMint(1, { value: fee });
 
     // View the minted NFT events
+    icons.on("TransferSingle", (operator, from, to, id, value) => {
+        console.log(operator, from, to, id, value);
+    });
 
     // Withdraw the MATIC and LINK from the contract
     await icons.withdraw();
