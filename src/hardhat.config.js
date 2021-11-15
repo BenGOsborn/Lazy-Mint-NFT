14e85555bb3c05d41b12d07dce38e6e2fab39920
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-15 11:00:58 +1100

    🎉 Update

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -19,6 +17,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
     solidity: "0.8.4",
     networks: {
