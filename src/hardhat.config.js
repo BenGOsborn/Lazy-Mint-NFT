dabce507d5b4049922ecc5e5fd97227a12135031
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 09:51:58 +1100

    🎉 Update

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -1,26 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
-// **** TRY TESTING ON A DIFFERENT BLOCKCHAIN WITH A DIFFERENT ADAPTER
-// **** USE THE SAME NETWORK AND ADAPTER AS THE ONE FROM THE TUTORIAL - https://docs.chain.link/docs/advanced-tutorial/
-// **** NEXT STEP: TRY DEPLOYING THE ORIGINAL TUTORIAL CONTRACT, THEN IF THAT DOESNT WORK, CHECK IT ON THE ORIGINAL NETWORK AND THEN SWITCH BACK
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const INFURA_KEY = process.env.INFURA_KEY;
