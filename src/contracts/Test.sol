ab0139ffa9ef4bf9fb2b17d560ca37ef0283e56f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-16 13:41:52 +1100

    🎉 Update

--- src/contracts/Test.sol
+++ src/contracts/Test.sol
@@ -6,6 +6,6 @@ import "./lib/verifyIPFS.sol";
 contract Test {
     function storeData(bytes32 _data) external returns (string memory ipfsHash) {
         bytes memory encoded = verifyIPFS.toBase58(abi.encodePacked(_data));
-        ipfsHash = string(abi.encodePacked("Qm", encoded));
+        ipfsHash = string(abi.encodePacked(encoded)); // **** How do I add the "Qm" to this ? (maybe I can make another chainlnik call recursively which will allow me to also get the hash function dynamically)
     }
 }
