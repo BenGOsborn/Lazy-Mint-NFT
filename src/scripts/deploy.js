a5b31b07ae41153dcff41eb675c0b9ff8165d847
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-16 20:50:41 +1100

    🎉 Update

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -6,6 +6,7 @@
 const hre = require("hardhat");
 const fs = require("fs");
 const bs58 = require("bs58");
+const ERC20ABI = require("@openzeppelin/contracts/build/contracts/ERC20.json");
 
 async function main() {
     // Hardhat always runs the compile task when running scripts with its command
@@ -43,6 +44,13 @@ async function main() {
     const FILENAME = "address.txt";
     fs.writeFileSync(FILENAME, icons.address);
     console.log("Saved address to " + FILENAME);
+
+    // Fund the contract with LINK
+    const signer = hre.ethers.provider.getSigner();
+    const LINK_ADDRESS = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+    const link = new hre.ethers.Contract(LINK_ADDRESS, ERC20ABI.abi, signer);
+    await link.transfer(icons.address, (1e18).toString());
+    console.log("Initialized LINK contract and sent LINK tokens");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
