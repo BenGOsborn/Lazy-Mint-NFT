910863d20bc9449e87900a31a547d224f7350b1a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 11:14:48 +1100

    🎉 Update

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -4,27 +4,22 @@ const bs58 = require("bs58");
 const ERC20ABI = require("@openzeppelin/contracts/build/contracts/ERC20.json");
 
 async function main() {
-    // Contract parameters
-    const MINT_FEE_PER_TOKEN = 10;
+    // Contract parameters (Polygon Mumbai Testnet)
+    const NAME = "Icons";
+    const SYMBOL = "ICON";
     const MAX_TOKENS = 1000;
-    const URI = "";
+    const MINT_FEE = 10;
     const EARLY_MINT_END = Math.floor((Date.now() + 3.6e6) / 1000);
-    // const ORACLE = "0x0bDDCD124709aCBf9BB3F824EbC61C87019888bb";
-    // const JOB_ID = hre.ethers.utils.id("c6a006e4f4844754a6524445acde84a0");
-    // const LINK_FEE = (0.1e18).toString();
-    // const API_URL = "https://lazy-nft.herokuapp.com/generate";
+    const ORACLE = "0xc8D925525CA8759812d0c299B90247917d4d4b7C";
+    const JOB_ID = hre.ethers.utils.id("a7330d0b4b964c05abc66a26307047c0");
+    const LINK_FEE = (0.01e18).toString();
+    const API_URL = "https://lazy-nft.herokuapp.com/generate";
     const LINK_ADDRESS = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 
     // Compile and deploy the contract
     await hre.run("compile");
-    // const Icons = await hre.ethers.getContractFactory("Icons");
-    const Icons = await hre.ethers.getContractFactory("APIConsumer");
-    // const icons = await Icons.deploy(MINT_FEE_PER_TOKEN, MAX_TOKENS, URI, EARLY_MINT_END, ORACLE, JOB_ID, LINK_FEE, API_URL, LINK_ADDRESS);
-    // const icons = await Icons.deploy(MINT_FEE_PER_TOKEN, MAX_TOKENS, URI, EARLY_MINT_END, LINK_ADDRESS);
-    const exampleUri = "QmcPYGnhRrumj8WGSRMm9j1yaH8p2n1rYgTJeu4hyxBADA";
-    const decodedUri = bs58.decode(exampleUri);
-    const prefix = hre.ethers.utils.hexZeroPad(`0x${decodedUri.slice(0, 2).toString("hex")}`, 32);
-    const icons = await Icons.deploy(prefix);
+    const Icons = await hre.ethers.getContractFactory("Icons");
+    const icons = await Icons.deploy(NAME, SYMBOL, MAX_TOKENS, MINT_FEE, EARLY_MINT_END, ORACLE, JOB_ID, LINK_FEE, API_URL, LINK_ADDRESS);
     await icons.deployed();
     console.log("Deployed contract " + icons.address);
 
