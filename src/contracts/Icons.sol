1106d12b807e0ebd09a73ddb33a580a8e9300f27
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 13:06:50 +1100

    🎉 Update

--- src/contracts/Icons.sol
+++ src/contracts/Icons.sol
@@ -105,11 +105,11 @@ contract Icons is Ownable, ERC721, ChainlinkClient {
         _mintIcon();
     }
 
-    function requestData() public returns (bytes32 requestId) {
+    function requestData() public {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill1.selector);
         request.add("get", "https://lazy-nft.herokuapp.com/generate?tokenId=3");
         request.add("path", "chunks.0");
-        return sendChainlinkRequestTo(oracle, request, linkFee);
+        sendChainlinkRequestTo(oracle, request, linkFee);
     }
 
     // Mint a new Icon
