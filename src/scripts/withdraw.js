77800fbe5338f8086636150a37791f403997ba5b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 11:17:56 +1100

    🎉 Update

--- src/scripts/withdraw.js
+++ src/scripts/withdraw.js
@@ -1,20 +1,17 @@
 const hre = require("hardhat");
-// const IconsABI = require("../artifacts/contracts/Icons.sol/Icons.json");
-const IconsABI = require("../artifacts/contracts/APIConsumer.sol/APIConsumer.json");
+const IconsABI = require("../artifacts/contracts/Icons.sol/Icons.json");
 const fs = require("fs");
 
 async function main() {
-    // Initialize the signer
-    const signer = hre.ethers.provider.getSigner();
-
     // Initialize the contract
+    const signer = hre.ethers.provider.getSigner();
     const FILENAME = "address.txt";
     const iconsAddress = fs.readFileSync(FILENAME, "utf8");
     const icons = new hre.ethers.Contract(iconsAddress, IconsABI.abi, signer);
     console.log("Initialized Icons contract from " + FILENAME);
 
     // Withdraw the MATIC and LINK from the contract
-    // await icons.withdraw();
+    await icons.withdraw();
     await icons.withdrawLink();
     console.log("Withdrew tokens from contract");
 }
