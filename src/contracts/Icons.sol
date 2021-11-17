2dcc618e2c629e27dd238e4a072b1fc9642d31ab
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 11:05:56 +1100

    🎉 Update

--- src/contracts/Icons.sol
+++ src/contracts/Icons.sol
@@ -5,11 +5,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract Icons is Ownable, ERC721, ChainlinkClient {
-    using SafeMath for uint256;
     using Chainlink for Chainlink.Request;
 
     // Chainlink data
