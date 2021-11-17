d23db1b25bc05971aa8d9c96c38a60f5606d8d98
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-17 17:05:59 +1100

    🎉 Update

--- src/scripts/view-minted.js
+++ src/scripts/view-minted.js
@@ -13,8 +13,9 @@ async function main() {
     // Get the events
     const filter = icons.filters.Transfer();
     const blockNumber = (await hre.ethers.provider.getBlock()).number;
-    // const events = await icons.queryFilter(filter, blockNumber - 1000, blockNumber);
-    const events = await icons.queryFilter(filter, 21566180, 21566190);
+    const events = await icons.queryFilter(filter, blockNumber - 1000, blockNumber);
+    // const events = await icons.queryFilter(filter, 21566180, 21566190);
+    console.log("Events:\n=======");
     for (const event of events) {
         console.log(event.args);
     }
